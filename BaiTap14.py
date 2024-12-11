6373e02b3835acca4a4e25d404fbc3c4269ced9a
Nguyễn Hoàng Tân <110407548+Dev-Aligator@users.noreply.github.com>
2024-12-11 22:01:59 +0700

    Update BaiTap14.py

--- BaiTap14.py
+++ BaiTap14.py
@@ -2,19 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import gdown
-import joblib
 
 # Tải mô hình và label_encoder từ Google Drive
 model_url = 'https://drive.google.com/uc?id=1UtVhI3XtBR-vhYMrnCkCsdcSRXot2dS4'
+model_path = 'diamond_price_model.pkl'
 label_encoder_url = 'https://drive.google.com/uc?id=1Y8E3DrkkjGXHk0GfIFzr6xDO_DCQ9mwQ'
+label_encoder_path = 'label_encoder.pkl'
 
 # Tải tệp từ Google Drive
-gdown.download(model_url, 'model.pkl', quiet=False)
-gdown.download(label_encoder_url, 'label_encoder.pkl', quiet=False)
+if not os.path.exists(model_path):
+    gdown.download(model_url, model_path, quiet=False)
 
-# Tải mô hình và label_encoder từ tệp .pkl
-model = joblib.load('model.pkl')
-label_encoder = joblib.load('label_encoder.pkl')
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+if not os.path.exists(label_encoder_path):
+    gdown.download(label_encoder_url, label_encoder_path, quiet=False)
+
+with open(label_encoder_path, 'rb') as file:
+    label_encoder = pickle.load(file)
 
 # Tạo giao diện Streamlit
 st.title('Dự đoán giá kim cương')
